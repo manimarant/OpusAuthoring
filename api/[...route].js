1e2312c0fc4af45ddab83721192eb3fb6e2db9ff
manimaran.t <manimarant>
2026-03-13 11:42:06 +0530

    fix: switch API bundle to CJS to resolve dynamic require errors

--- api/[...route].js
+++
@@ -1,4 +0,0 @@
-// This file is a placeholder. It will be overwritten by the build process.
-export default async function handler(req, res) {
-  res.status(500).send("Build error: API bundle not found.");
-}
