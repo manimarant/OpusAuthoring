b14374a1a5e4a68ad65a9e495a092d95749453d5
manimaran.t <manimarant>
2026-03-13 11:33:35 +0530

    fix: provide placeholder for Vercel function discovery

---
+++ api/[...route].js
@@ -0,0 +1,4 @@
+// This file is a placeholder. It will be overwritten by the build process.
+export default async function handler(req, res) {
+  res.status(500).send("Build error: API bundle not found.");
+}
